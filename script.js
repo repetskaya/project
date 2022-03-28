a919671b7c1aa979eb9c8bee943b7d017c8efe0f
Bogdana <dana@mac.Home>
2022-03-28 14:28:54 +0100

    commit

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-let number = 5;
-const leftBorderWidth = 2;
+const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "8");
 
-number = 10;
-console.log(number);
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors:{},
+  genres:[],
+  privat:false
+};
+
+const lastSeenFilm = prompt("Один из последних просмотренных фильмов?", ""); // hello
+const rateIt = prompt("На сколько оцените его?", ""); // 8
+
+const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+const rateFilm = prompt("На сколько оцените его?", "");
+
+
+// const movies = {
+//   lastSeenFilm: rateIt,
+//   [lastFilm]: rateFilm,
+// };
+
+personalMovieDB.movies[lastSeenFilm] = rateIt;
+
+console.log(personalMovieDB);
