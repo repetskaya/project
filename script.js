b0119260240ad4528cd93be3025e14ee1342a08c
Bogdana <dana@mac.Home>
2022-03-31 16:44:39 +0100

    functions

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "8");
+// const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "8");
 
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors:{},
-  genres:[],
-  privat:false
-};
-
-const lastSeenFilm = prompt("Один из последних просмотренных фильмов?", ""); // hello
-const rateIt = prompt("На сколько оцените его?", ""); // 8
-
-const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-const rateFilm = prompt("На сколько оцените его?", "");
-
-
-// const movies = {
-//   lastSeenFilm: rateIt,
-//   [lastFilm]: rateFilm,
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors:{},
+//   genres:[],
+//   privat:false
 // };
 
-personalMovieDB.movies[lastSeenFilm] = rateIt;
+// const lastSeenFilm = prompt("Один из последних просмотренных фильмов?", ""); // hello
+// const rateIt = prompt("На сколько оцените его?", ""); // 8
 
-console.log(personalMovieDB);
+// const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+// const rateFilm = prompt("На сколько оцените его?", "");
+
+
+// // const movies = {
+// //   lastSeenFilm: rateIt,
+// //   [lastFilm]: rateFilm,
+// // };
+
+// personalMovieDB.movies[lastSeenFilm] = rateIt;
+
+// console.log(personalMovieDB);
+
+let isLoop = true;
+
+while (isLoop) {
+  const lastSeenFilm = prompt("Один из последних просмотренных фильмов?", "");
+
+  if(lastSeenFilm.length !== 0 && lastSeenFilm.length < 50) {
+    isLoop = false;
+  }
+
+}
